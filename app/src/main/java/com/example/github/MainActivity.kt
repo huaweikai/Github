cf74abe68085350631e9449a1d5b9b408d55f567
huaweikai <1297720454@qq.com>
2021-06-09 02:57:18 +0800

    add a button for '+1' to layout
    这是用来做简单计数的按键

--- app/src/main/java/com/example/github/MainActivity.kt
+++ app/src/main/java/com/example/github/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.github
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button: Button=findViewById(R.id.button);
+        val text:TextView=findViewById(R.id.textview);
+        var sum:Int=0;
+        button.setOnClickListener{
+            text.text= (sum++).toString()
+        }
     }
 }
