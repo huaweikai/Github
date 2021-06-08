a07df39b6a529b86cb91fd5b2f7ad32c01ae77f0
huaweikai <1297720454@qq.com>
2021-06-09 03:47:02 +0800

    fix data lose

--- app/src/main/java/com/example/github/MainActivity.kt
+++ app/src/main/java/com/example/github/MainActivity.kt
@@ -2,23 +2,31 @@ package com.example.github
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var sum=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button: Button=findViewById(R.id.button);
         val button2: Button=findViewById(R.id.button2);
         val text:TextView=findViewById(R.id.textview);
-        var sum:Int=0;
-        text.text="0"
+        sum=savedInstanceState?.getInt("NUMBER")?:0
+        text.text= sum.toString()
         button.setOnClickListener{
             text.text= (sum++).toString()
         }
         button2.setOnClickListener{
             text.text= (sum--).toString()
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("NUMBER",sum)
     }
 }
